addce63155c17e1f12816a7137f3606e8b6db49c
akhildevelops <akhilgodvsdemon@gmail.com>
2024-08-22 07:44:26 +0500

    remove all non python related files

--- example/hf-hub/Readme.md
+++
@@ -1,4 +0,0 @@
-# Huggingface Model download example
-
-Test run this example by going to `example/hf-hub` and run `zig build run`
-
--- huggingface/lib.zig
+++
@@ -1,4 +0,0 @@
-const _artifacts = @import("artifacts.zig");
-pub const utils = @import("utils.zig");
-pub const Repo = _artifacts.Repo;
-pub const Cache = _artifacts.Cache;
