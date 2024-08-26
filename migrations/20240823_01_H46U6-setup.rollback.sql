f61ba67c6d03617e90566535c971d39a27dc4179
akhildevelops <akhilgodvsdemon@gmail.com>
2024-08-26 11:41:12 +0530

    db setup

--- migrations/20240823_01_H46U6-setup.rollback.sql
+++ migrations/20240823_01_H46U6-setup.rollback.sql
@@ -1,4 +1,26 @@
 -- setup
 -- depends:
 
-DROP TABLE IF EXISTS LIGHT;
+-- people module
+DROP TABLE IF EXISTS people_activity;
+
+DROP TABLE IF EXISTS people;
+-----------------------------------
+
+--light module
+DROP TABLE IF EXISTS light_activity;
+
+DROP TABLE IF EXISTS light;
+-----------------------------------
+
+--camera module
+DROP TABLE IF EXISTS camera_activity;
+
+DROP TABLE IF EXISTS camera;
+-----------------------------------
+
+-- module
+DROP TABLE IF EXISTS module;
+
+-- Status Type
+DROP TYPE IF EXISTS STATUS;
--- migrations/20240823_01_H46U6-setup.sql
+++ migrations/20240823_01_H46U6-setup.sql
@@ -1,7 +1,70 @@
 -- setup
 -- depends:
 
-CREATE TABLE IF NOT EXISTS LIGHT (
-    id SERIAL UNIQUE PRIMARY KEY,
-    name TEXT
+CREATE TYPE STATUS AS ENUM('yes', 'no');
+
+-- module
+CREATE TABLE IF NOT EXISTS module (
+    id INTEGER UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO
+    module (id, name)
+VALUES (0, 'camera'),
+    (1, 'light'),
+    (2, 'people');
+-----------------------------------
+
+-- Camera Module
+CREATE TABLE IF NOT EXISTS camera (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    module_id INTEGER NOT NULL REFERENCES module (id) DEFAULT 0,
+    present_at VARCHAR(100) NOT NULL,
+    url VARCHAR(500)
+);
+
+CREATE TABLE IF NOT EXISTS camera_activity (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    camera_id INTEGER NOT NULL REFERENCES camera (id)
+);
+-----------------------------------
+
+-- Light Module
+CREATE TABLE IF NOT EXISTS light (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    module_id INTEGER NOT NULL REFERENCES module (id) DEFAULT 1,
+    camera_id INTEGER REFERENCES camera (id),
+    located_at VARCHAR(100) NOT NULL,
+    capture_interval INTEGER DEFAULT 60
+);
+
+CREATE TABLE IF NOT EXISTS light_activity (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    captured_at TIMESTAMP WITH TIME ZONE,
+    light_id INTEGER NOT NULL REFERENCES light (id),
+    switched_on STATUS NOT NULL
+);
+-----------------------------------
+
+-- People Module
+CREATE TABLE IF NOT EXISTS people (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    module_id INTEGER NOT NULL REFERENCES module (id) DEFAULT 2,
+    present_at VARCHAR(100) NOT NULL,
+    capture_interval INTEGER DEFAULT 30
+);
+
+CREATE TABLE IF NOT EXISTS people_activity (
+    id SERIAL UNIQUE PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    people_id INTEGER NOT NULL REFERENCES people (id),
+    no_of_people INTEGER DEFAULT 0
+);
+-----------------------------------
